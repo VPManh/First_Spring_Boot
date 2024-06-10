631c6eee96c73313f17583113557e0f6db1d2a54
Phu Manh <vanphumanh14042003@gmail.com>
2024-06-10 19:27:30 +0700

    update partical

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,8 +8,15 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World with Mạnh Văn !";
-
-}
+        return "Hello Mạnh Văn 2003!";
+    }
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
+    }
 
 }
