9a1e30f72d6358c3b097608de6bdc3e7f255d593
Phu Manh <vanphumanh14042003@gmail.com>
2024-06-10 21:52:34 +0700

    remove auto security login,out

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -3,7 +3,10 @@ package vn.hoidanit.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+// exclude : có nghĩa là loại bỏ
+@SpringBootApplication(
+	exclude =  org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
