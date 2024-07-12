9c85c6a99de50babc5886f7d965a4e963b7d058f
Phu Manh <vanphumanh14042003@gmail.com>
2024-07-12 15:42:02 +0700

    show cart detail

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -1,11 +1,17 @@
 package vn.hoidanit.laptopshop.controller.client;
 
+import java.util.List;
+
+import org.eclipse.tags.shaded.org.apache.regexp.recompile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.domain.Cart;
+import vn.hoidanit.laptopshop.domain.CartDetail;
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.ProductService;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -44,7 +50,22 @@ public class ItemController {
     }
 
     @GetMapping("/cart")
-    public String getCartPage() {
+    public String getCartPage(Model model, HttpServletRequest request) {
+        User currentUser = new User();
+        HttpSession session = request.getSession(false);
+        long id = (long)session.getAttribute("id");
+        currentUser.setId(id);
+
+        Cart cart = this.productService.fetchByUser(currentUser);
+
+        List<CartDetail> cartDetails = cart.getCartDetails();
+        double totalPrice = 0;
+        for (CartDetail cartDetail : cartDetails) {
+            totalPrice = totalPrice + cartDetail.getPrice() * cartDetail.getQuantity();
+        }
+        
+        model.addAttribute("totalPrice",totalPrice);
+        model.addAttribute("cartDetails", cartDetails);
         return "client/cart/show";
     }
     
