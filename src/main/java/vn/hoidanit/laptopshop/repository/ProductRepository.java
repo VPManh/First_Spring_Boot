3129c9f321123c4561b7e310c0dcc1bf24c0ea70
Phu Manh <vanphumanh14042003@gmail.com>
2024-07-26 11:25:47 +0700

    pagination complete fetch product

--- src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package vn.hoidanit.laptopshop.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import vn.hoidanit.laptopshop.domain.Product;
+
 import java.util.Optional;
 
 
@@ -13,6 +16,6 @@ public interface ProductRepository extends JpaRepository<Product ,Long>{
     Product save(Product product);
     Optional<Product> findById(long id);
     Product deleteById(long id);
+    Page<Product> findAll(Pageable pageable);
 
-    
 }
